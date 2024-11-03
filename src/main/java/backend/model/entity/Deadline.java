71922a6458f04be09ff2c5bb91e978b53f3253e8
JimGu <gwt200407@gmail.com>
2024-11-03 09:41:33 +0800

    Fix incorrect field type

--- src/main/java/backend/model/entity/Deadline.java
+++ src/main/java/backend/model/entity/Deadline.java
@@ -5,8 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 /**
  * 截止日期表
+ * 
  * @field id 主键 int
  * @field time 截止日期 DateTime
  * @field type 截止日期类型 int (0 — 提交个人材料，1 — 第一轮导师选择学生，2 — 第二轮调剂，3 — 录取结束)
@@ -20,7 +23,7 @@ public class Deadline {
 
     private Integer id;
 
-    private String time;
+    private LocalDateTime time;
 
     // 0 — 提交个人材料，1 — 第一轮导师选择学生，2 — 第二轮调剂，3 — 录取结束
     private Integer type;
