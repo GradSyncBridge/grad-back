2761ba2d84863fa72c9b09ede4a9c64c186ffc26
JimGu <gwt200407@gmail.com>
2024-11-07 16:34:37 +0800

    Concise error message

--- src/main/java/backend/exception/model/teacher/TeacherNotFoundException.java
+++ src/main/java/backend/exception/model/teacher/TeacherNotFoundException.java
@@ -1,11 +1,16 @@
 package backend.exception.model.teacher;
 
 import backend.exception.model.BaseException;
+import backend.model.entity.Teacher;
 
 public class TeacherNotFoundException extends BaseException {
 
     public TeacherNotFoundException() {
-        super("Teacher not found: ", 401);
+        super("Major not found", 404);
+    }
+
+    public TeacherNotFoundException(Integer mid) {
+        super("Major with id = " + mid + " is not found.", 404);
     }
 
 }
