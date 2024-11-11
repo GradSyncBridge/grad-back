ac872903f89f53051340b8c04eebedd5cd781850
JimGu <gwt200407@gmail.com>
2024-11-11 20:05:59 +0800

    Refactor deadline & establish deadline enum

---
+++ src/main/java/backend/Enums/DeadlineEnum.java
@@ -0,0 +1,21 @@
+package backend.Enums;
+
+import lombok.Getter;
+
+@Getter
+public enum DeadlineEnum {
+    INITIAL_SUBMISSION(0, "initial submission"),
+    SECOND_SUBMISSION(1, "second submission"),
+    FIRST_ENROLL(2, "first enroll"),
+    SECOND_ENROLL(3, "second enroll"),
+    THIRD_ENROLL(4, "third enroll");
+
+    private final int value;
+    private final String description;
+
+    DeadlineEnum(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+}
--- src/main/java/backend/model/entity/Deadline.java
+++ src/main/java/backend/model/entity/Deadline.java
@@ -25,6 +25,6 @@ public class Deadline {
 
     private LocalDateTime time;
 
-    // 0 — 提交个人材料，1 — 第一轮导师选择学生，2 — 第二轮调剂，3 — 录取结束
+    // 0: 提交初试材料截止；1: 提交复试材料截止；2: 第一轮录取结束；3: 第二轮录取结束；4: 第三轮录取结束
     private Integer type;
 }
