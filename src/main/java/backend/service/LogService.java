8c8adccd48c5b159e554284b9aba4abe75e4b2a8
cheng-lao <3431438564@qq.com>
2024-11-19 11:25:02 +0800

    添加logService方法注释

--- src/main/java/backend/service/LogService.java
+++ src/main/java/backend/service/LogService.java
@@ -6,7 +6,17 @@ import java.util.List;
 import org.aspectj.lang.ProceedingJoinPoint;
 
 public interface LogService {
+    /**
+     * 获取所有的日志信息
+     * GET /log
+     * @return 日志列表
+    * */
     List<LogVO> getLog();
 
+    /**
+     * 拦截getthis方法，插入日志信息
+     * @param joinPoint 拦截方法 backend.model.LogDTO.getthis()方法
+     * @return getthis方法返回内容
+     * */
     Object logging(ProceedingJoinPoint joinPoint);
 }
