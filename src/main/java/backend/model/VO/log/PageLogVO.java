d5ebdcbe018a855bf0462b033dc0e07d04c861cf
= <qinglinhe44@gmail.com>
2024-11-30 16:08:37 +0800

    Add pagination query

---
+++ src/main/java/backend/model/VO/log/PageLogVO.java
@@ -0,0 +1,20 @@
+package backend.model.VO.log;
+
+import backend.model.VO.log.LogVO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class PageLogVO {
+
+    Integer total;
+
+    List<LogVO> logs;
+}
