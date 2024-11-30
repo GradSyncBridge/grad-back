e3154b6bfdb0f670efa54f18758434ab6c40dcef
= <qinglinhe44@gmail.com>
2024-11-30 15:22:41 +0800

    Simplify the code

--- src/test/java/backend/GradSyncBridgeApplicationTests.java
+++ src/test/java/backend/GradSyncBridgeApplicationTests.java
@@ -1,49 +1,16 @@
 package backend;
 
-import backend.model.DTO.EnrollConfirmDTO;
-import backend.service.impl.EnrollServiceImpl;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-import java.util.concurrent.CompletableFuture;
 
 @EnableAspectJAutoProxy
 @SpringBootTest
 class GradSyncBridgeApplicationTests {
 
-    @Autowired
-    private EnrollServiceImpl enrollService;
     @Test
     void contextLoads() {
-        EnrollConfirmDTO.builder().studentID(47).majorID(2).build();
 
-        CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> {
-            try {
-                enrollService.enrollConfirmById(EnrollConfirmDTO.builder().studentID(59).majorID(2).build(), 3);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        CompletableFuture<Void> future2 =CompletableFuture.runAsync(() -> {
-            try {
-                enrollService.enrollConfirmById(EnrollConfirmDTO.builder().studentID(59).majorID(2).build(), 4);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        CompletableFuture<Void> future3 =CompletableFuture.runAsync(() -> {
-            try {
-                enrollService.enrollConfirmById(EnrollConfirmDTO.builder().studentID(59).majorID(2).build(), 5);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        CompletableFuture.allOf(future1, future2, future3).join();
     }
 
 }
