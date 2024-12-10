254b230036a509a6d2e342349433f21167cd38e2
JimGu <gwt200407@gmail.com>
2024-12-10 11:17:40 +0800

    Fix incorrect comments

--- src/main/java/backend/service/MajorService.java
+++ src/main/java/backend/service/MajorService.java
@@ -8,18 +8,18 @@ import java.util.List;
 
 public interface MajorService {
     /**
-     * 获取对应学院下的一级学科
-     * /unauthorized/catalogue/second
+     * 获取对应学院下的学科
+     * /unauthorized/catalogue
      * @param department 学院
      * @return 一级学科列表
      */
     List<MajorVO> getCatalogue(Integer department);
 
     /**
-     * 获取对应一级学科下的二级学科
-     * GET /unauthorized/catalogue/second
-     * @param department 一级学科
-     * @return 二级学科列表
+     * 获取对应学院下的一级学科
+     * GET /unauthorized/catalogue/first
+     * @param department 学院ID
+     * @return 一级学科列表
      */
     List<MajorFirstVO> getFirstMajorByDept(Integer department);
 
